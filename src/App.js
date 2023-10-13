f844867c0efc5dc77cabb7da3640a3ed9ad3e424
VoidVampire <yashdsingh2004@gmail.com>
2023-10-13 16:54:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const url = 'https://dummyjson.com/users';
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setUsers(data.users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dummy data</h1>
+      <div className="data">
+        <table>
+          <thead>
+            <tr>
+              <th>Sno</th>
+              <th>Profile Pic</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>E-mail</th>
+              <th>Username</th>
+              <th>Domain</th>
+              <th>IP</th>
+              <th>University</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  <img src={user.image} alt={user.username} height="45px" />
+                </td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.gender}</td>
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+                <td>{user.domain}</td>
+                <td>{user.ip}</td>
+                <td>{user.university}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
